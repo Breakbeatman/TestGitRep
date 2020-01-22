4e3972ca5e9707af52e1da41538cea922b83eec2
blazerlover <pashkovich1@yandex.ru>
2020-01-22 20:00:11 +0300

    First commit
    test

---
+++ app/src/main/java/ru/exemple/testgitrep/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.exemple.testgitrep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
